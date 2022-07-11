a3063544433aee4c603092e5562e9b9bb331837c
Sourcery AI <>
2022-07-11 16:04:43 +0000

    'Refactored by Sourcery'

--- asgi_webdav/config.py
+++ asgi_webdav/config.py
@@ -49,12 +49,15 @@ class Provider(BaseModel):
     readonly: bool = False  # TODO impl
 
 
+
+
 class GuessTypeExtension(BaseModel):
     enable: bool = True
     enable_default_mapping: bool = True
 
-    filename_mapping: dict = dict()
-    suffix_mapping: dict = dict()
+    filename_mapping: dict = {}
+    suffix_mapping: dict = {}
+
 
 
 class TextFileCharsetDetect(BaseModel):
@@ -82,10 +85,13 @@ class CORS(BaseModel):
     preflight_max_age: int = 600
 
 
+
+
 class HideFileInDir(BaseModel):
     enable: bool = True
     enable_default_rules: bool = True
-    user_rules: dict[str, str] = dict()
+    user_rules: dict[str, str] = {}
+
 
 
 class LoggingLevel(Enum):
@@ -135,11 +141,14 @@ class Config(BaseModel):
             password = None
 
         if username is not None:
-            user_id = None
-            for index in range(len(self.account_mapping)):
-                if self.account_mapping[index].username == username:
-                    user_id = index
-                    break
+            user_id = next(
+                (
+                    index
+                    for index in range(len(self.account_mapping))
+                    if self.account_mapping[index].username == username
+                ),
+                None,
+            )
 
             if user_id is None:
                 account = User(username=username, password=password, permissions=["+"])
@@ -161,13 +170,16 @@ class Config(BaseModel):
 
         # provider - CLI
         if aep.root_path is not None:
-            root_path_index = None
-            for index in range(len(self.provider_mapping)):
-                if self.provider_mapping[index].prefix == "/":
-                    root_path_index = index
-                    break
+            root_path_index = next(
+                (
+                    index
+                    for index in range(len(self.provider_mapping))
+                    if self.provider_mapping[index].prefix == "/"
+                ),
+                None,
+            )
 
-            root_path_uri = "file://{}".format(aep.root_path)
+            root_path_uri = f"file://{aep.root_path}"
             if root_path_index is None:
                 self.provider_mapping.append(Provider(prefix="/", uri=root_path_uri))
             else:
@@ -179,23 +191,20 @@ class Config(BaseModel):
 
         # response - default
         if self.guess_type_extension.enable_default_mapping:
-            new_mapping = dict()
-            new_mapping.update(DEFAULT_FILENAME_CONTENT_TYPE_MAPPING)
+            new_mapping = {}
+            new_mapping |= DEFAULT_FILENAME_CONTENT_TYPE_MAPPING
             new_mapping.update(self.guess_type_extension.filename_mapping)
             self.guess_type_extension.filename_mapping = new_mapping
 
-            new_mapping = dict()
-            new_mapping.update(DEFAULT_SUFFIX_CONTENT_TYPE_MAPPING)
+            new_mapping = {}
+            new_mapping |= DEFAULT_SUFFIX_CONTENT_TYPE_MAPPING
             new_mapping.update(self.guess_type_extension.suffix_mapping)
             self.guess_type_extension.suffix_mapping = new_mapping
 
-        # other - env
-        logging_level = getenv("WEBDAV_LOGGING_LEVEL")
-        if logging_level:
+        if logging_level := getenv("WEBDAV_LOGGING_LEVEL"):
             self.logging_level = LoggingLevel(logging_level)
 
-        sentry_dsn = getenv("WEBDAV_SENTRY_DSN")
-        if sentry_dsn:
+        if sentry_dsn := getenv("WEBDAV_SENTRY_DSN"):
             self.sentry_dsn = sentry_dsn
 
 
@@ -226,11 +235,11 @@ def init_config_from_file(config_file: str) -> Config:
         _config = _config.parse_file(config_file)
 
     except (FileNotFoundError, json.JSONDecodeError) as e:
-        message = "Load config value from file[{}] failed!".format(config_file)
+        message = f"Load config value from file[{config_file}] failed!"
         logger.warning(message)
         logger.warning(e)
 
-    logger.info("Load config value from config file:{}".format(config_file))
+    logger.info(f"Load config value from config file:{config_file}")
     return _config
 
 
